75b185cdb72c2b7c88109ee0eb53ad4df72b51b2
Gareth Healy <garethahealy@gmail.com>
2016-09-01 10:13:02 +0100

    Added type resolvers which are used by the new client response to give hints to jackson for converting objects easier

---
+++ src/main/java/org/hawkular/client/core/typeresolvers/MapJavaTypeResolver.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.client.core.typeresolvers;
+
+import java.util.Map;
+
+import org.hawkular.client.core.jaxrs.fasterxml.jackson.ClientObjectMapper;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MapJavaTypeResolver {
+
+    private final ObjectMapper objectMapper;
+
+    public MapJavaTypeResolver() {
+        this.objectMapper = new ClientObjectMapper();
+    }
+
+    /**
+     * Map with Generic Key and Value, i.e.: Map<String, TaggedBucketPoint>
+     */
+    public JavaType get(Class<? extends Map> mapClazz, Class<?> mapClazzKey, Class<?> mapClazzValue) {
+        JavaType mapClazzKeyType = objectMapper.getTypeFactory().constructType(mapClazzKey);
+        JavaType mapClazzValueType = objectMapper.getTypeFactory().constructType(mapClazzValue);
+
+        return objectMapper.getTypeFactory().constructMapType(mapClazz, mapClazzKeyType, mapClazzValueType);
+    }
+
+    /**
+     * Map with Generic Key and Genric Value, i.e.: Map<String, List<TaggedBucketPoint>>
+     */
+    public JavaType get(Class<? extends Map> mapClazz, Class<?> mapClazzKey, Class<?> mapClazzValue, Class<?> mapClazzParametrizedValue) {
+        JavaType mapClazzKeyType = objectMapper.getTypeFactory().constructType(mapClazzKey);
+        JavaType parametrizedClazzType = objectMapper.getTypeFactory().constructParametrizedType(mapClazzValue, mapClazzValue, mapClazzParametrizedValue);
+
+        return objectMapper.getTypeFactory().constructMapType(mapClazz, mapClazzKeyType, parametrizedClazzType);
+    }
+}
